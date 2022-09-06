258d2fda036702f64c3acc655c52a95f0af7d6b1
sanskargupta <sanskarg348@gmail.com>
2022-09-06 22:10:14 +0530

    added change/remove background and create media response

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,35 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-	docs: ["intro"],
+	docs: [
+		"intro",
+		{
+			type: "category",
+			label: "Chart-data-utils",
+			link: {
+				type: "generated-index",
+			},
+			collapsed: true,
+			items: [
+				"chart-data-utils/create-data",
+			],
+		},
+		{
+			type: "category",
+			label: "Image-utils",
+			link: {
+				type: "generated-index",
+			},
+			collapsed: true,
+			items: [
+				"image-utils/create-media-response",
+				"image-utils/parse-uploaded-file",
+				"image-utils/change-background",
+				"image-utils/remove-background"
+			],
+		},
+
+	],
 };
 
 module.exports = sidebars;
